c403af839908ad0dbd6426337f03d7d5e4738703
hellokingwei <wlgcww001@163.com>
2016-10-25 21:05:55 +0800

    Add files via upload

--- mt_core/backends/virtualbox.py
+++ mt_core/backends/virtualbox.py
@@ -1,5 +1,20 @@
-# coding=UTF-8
+# coding=utf-8
 
+#import os
+from vboxapi import VirtualBoxManager     #引入
 # TODO 后台组:启动VirtualBox虚拟机
-def power_on(vm_path):
-    pass
+def power_on():
+    mgr = VirtualBoxManager(None, None)
+    vbox = mgr.vbox
+    name = "XP"
+    mach = vbox.findMachine(name)
+    session = mgr.mgr.getSessionObject(vbox)
+    progress = mach.launchVMProcess(session, "gui", "")
+    progress.waitForCompletion(-1)
+    console = session.console
+ #   os.system("cd C:/Program Files/Oracle/VirtualBox/ && dir")
+ #   os.system("cd C:/Program Files/Oracle/VirtualBox/ && VirtualBox.exe")
+
+power_on()
+
+
