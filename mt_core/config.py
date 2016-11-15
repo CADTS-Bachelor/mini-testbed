0b6a4608a6fe893a5aef3366118d0b11acab3316
Li Rui <lirui3252007012@gmail.com>
2016-11-15 19:31:57 +0800

    add config.py.

---
+++ mt_core/config.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+
+class Config:
+    def __init__(self):
+        self.hypervisor_type = None
+
+    def load(self):
+        # TODO load from config/mt.cfg use ConfigParser
+        pass
